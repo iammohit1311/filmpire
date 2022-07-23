00539f53a65211819425c5cfcde2f05cf10ac9d1
Mohit Srivastava <iammohitpro@gmail.com>
2022-07-24 02:55:54 +0530

    first commit

---
+++ src/components/Alan.jsx
@@ -0,0 +1,56 @@
+import React, { useEffect, useContext } from "react";
+import alanBtn from "@alan-ai/alan-sdk-web";
+import { useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
+
+import {
+  selectGenreOrCategory,
+  searchMovie,
+} from "../features/currentGenreOrCategory";
+import { fetchToken } from "../utils";
+import { ColorModeContext } from "../utils/ToggleColorMode";
+
+const useAlan = () => {
+  const { setMode } = useContext(ColorModeContext);
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  useEffect(() => {
+    alanBtn({
+      key: "c758fcf0e2049da074c78af257721af02e956eca572e1d8b807a3e2338fdd0dc/stage",
+      onCommand: ({ command, mode, genres, genreOrCategory, query }) => {
+        if (command === "chooseGenre") {
+          const foundGenre = genres.find(
+            (g) => g.name.toLowerCase() === genreOrCategory.toLowerCase()
+          );
+
+          if (foundGenre) {
+            history.push("/");
+            dispatch(selectGenreOrCategory(foundGenre.id));
+          } else {
+            const category = genreOrCategory.startsWith("top")
+              ? "top_rated"
+              : genreOrCategory;
+            history.push("/");
+            dispatch(selectGenreOrCategory(category));
+          }
+        } else if (command === "changeMode") {
+          if (mode === "light") {
+            setMode("light");
+          } else {
+            setMode("dark");
+          }
+        } else if (command === "login") {
+          fetchToken();
+        } else if (command === "logout") {
+          localStorage.clear();
+          history.push("/");
+        } else if (command === "search") {
+          dispatch(searchMovie(query));
+        }
+      },
+    });
+  }, []);
+};
+
+export default useAlan;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import App from "./components/App";
+import store from "./app/store";
+import "./index.css";
+import ToggleColorModeProvider from "./utils/ToggleColorMode";
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ToggleColorModeProvider>
+      <BrowserRouter basename="/filmpire">
+        <App />
+      </BrowserRouter>
+    </ToggleColorModeProvider>
+  </Provider>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
